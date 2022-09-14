68eeb4a7e7b71f5aec146b57a7bd986d7570ea9e
Sobhan.SRZA <90289153+Sobhan-SRZA@users.noreply.github.com>
2022-09-15 03:30:30 +0430

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,5 +2,7 @@ echo off
 color 1
 cls
 :a
-npm install dotenv discord.js cli-color cpu-stat moment os quick.db fs express
+
+npm install quick.db passport-discord axios body-parser	cli-color cpu-stat discord.js dotenv ejs express express-session fs memorystore moment ms os passport	
+
 goto a
