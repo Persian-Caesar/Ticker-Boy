a1c67d2943bc3fb0de142195ccb5a005eb82deb7
Sobhan <90289153+Sobhan-SRZA@users.noreply.github.com>
2022-12-25 12:03:27 +0330

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,5 +2,5 @@ echo off
 color 1
 cls
 :a
-npm install dotenv discord.js cli-color cpu-stat moment os quick.db fs express
+npm install dotenv discord.js@^13 cli-color cpu-stat moment os quick.db@^7 fs express
 goto a
